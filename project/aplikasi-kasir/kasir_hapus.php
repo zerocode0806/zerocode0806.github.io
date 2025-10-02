0668294c2d9c07810d111a15d750852b0f69f53a
zerocode0806 <ubaidillahdahlan@gmail.com>
2025-10-02 20:22:11 +0700

    update link repo
    delete project that already exist in repo

--- project/aplikasi-kasir/kasir_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $id = $_GET['id'];
-    $query = mysqli_query($koneksi, "DELETE FROM pelanggan WHERE id_pelanggan=$id");
-    if ($query) {
-        echo "<script>alert('Data berhasil di hapus'); location.href = '?page=kasir';</script>";
-    } else {
-        echo "<script>alert('Data gagal di hapus'); location.href = '?page=kasir';</script>";
-    }
-?>
